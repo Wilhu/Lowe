70a481be78da5cfdd6259af1ed5b95308e087cb8
Wilhu <toni.holappa@hotmail.fi>
2021-04-23 14:02:33 +0300

    dev branch

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -11,12 +11,25 @@ public class playerMovement : MonoBehaviour
     public float acceleration = 0.5f;
     public float deceleration = 1.5f;
     public float turnRate = 1;
+    private float humanMovementSpeedMax;
+    private float humanJumpForce;
+    private float humanAcceleration;
+    private float humanDeceleration;
+    private float humanTurnRate;
+    public float bearMovementSpeedMax;
+    public float bearJumpForce;
+    public float bearAcceleration;
+    public float bearDeceleration;
+    public float bearTurnRate;
     private Rigidbody2D playerRigidbody;
     private BoxCollider2D playerBoxCollider;
     [SerializeField] private LayerMask platformLayerMask;
     private bool playerIsFacingRight = true;
     private CapsuleCollider2D playerCapsuleCollider;
     public float useGravity;
+    bool BearCheck = false;
+    public float bearBuffTimer = 5;
+    public float mayJump = 0;
 
 
 
@@ -25,12 +38,19 @@ public class playerMovement : MonoBehaviour
         playerRigidbody = GetComponent<Rigidbody2D>();
         playerBoxCollider = GetComponent<BoxCollider2D>();
         playerCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        humanMovementSpeedMax = movementSpeedMax;
+        humanJumpForce = jumpForce;
+        humanAcceleration = acceleration;
+        humanDeceleration = deceleration;
+        humanTurnRate = turnRate;
     }
 
     void FixedUpdate()
     {
+
         float movement = Input.GetAxis("Horizontal");
-        Debug.Log(movementSpeed);
+        mayJump += Time.deltaTime;
+        //Debug.Log(mayJump);
        
          
         playerRigidbody.gravityScale = 0;
@@ -66,19 +86,55 @@ public class playerMovement : MonoBehaviour
             //Debug.Log("Standing");
         }
 
-        Debug.Log(movement);
+        //Debug.Log(movement);
 
         transform.position = transform.position + new Vector3(movement, 0, 0) * movementSpeed * Time.deltaTime;
+
+        if(BearCheck==true && Input.GetButton("Fire1"))
+        {
+            Debug.Log("karhu");
+            StartCoroutine(BearBuff());
+        }
+/*
+         if(Input.GetButtonDown("Jump"))
+        {
+            if(IsGrounded())
+            {
+            playerRigidbody.velocity = Vector3.zero;
+            playerRigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+
+        }
+        
+        if(IsGrounded())
+        {
+            mayJump = 0;
+        }
+*/
 
     }
 
 
     private void Update() {
 
-        float movement = Input.GetAxis("Horizontal");
-         if(IsGrounded() && Input.GetButtonDown("Jump"))
-        {
+        //float movement = Input.GetAxis("Horizontal");
+        //Debug.Log(mayJump);
+         if(/*mayJump > 0.5 ||*/ IsGrounded())
+         {
+          if(Input.GetButtonDown("Jump"))
+          {
+              mayJump = 0;
+              //Debug.Log(IsGrounded());
+              //Debug.Log("Jump");
+              //Debug.Log(mayJump);
+            playerRigidbody.velocity = Vector3.zero;
             playerRigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+          }
+         }
+        if(IsGrounded())
+        {
+            mayJump = 0;
         }
     }
 
@@ -96,7 +152,7 @@ public class playerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        float extraHeight  = 0.1f;
+        float extraHeight  = 0.05f;
         RaycastHit2D raycastHit = Physics2D.BoxCast(playerBoxCollider.bounds.center, playerBoxCollider.bounds.size - new Vector3(0.4f,0f,0f), 0f, Vector2.down, extraHeight, platformLayerMask);
         Color rayColor;
         if (raycastHit.collider != null)
@@ -113,8 +169,6 @@ public class playerMovement : MonoBehaviour
         Debug.DrawRay(playerBoxCollider.bounds.center - new Vector3(playerBoxCollider.bounds.extents.x, playerBoxCollider.bounds.extents.y + extraHeight), Vector2.right * (playerBoxCollider.bounds.size.x), rayColor);
       //  Debug.Log(raycastHit.collider);
         return raycastHit.collider != null;
-        
-
     }
 
     private void FlipPlayer()
@@ -126,4 +180,42 @@ public class playerMovement : MonoBehaviour
         transform.localScale = playerScale;
 
     }
-}
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+
+        if(other.tag == "Bear")
+        {
+            //Debug.Log("karhu");
+            BearCheck = true;
+        }
+            
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+
+        BearCheck = false;
+        //Debug.Log("ei karhua");
+    }
+
+    IEnumerator BearBuff()
+    {
+        Debug.Log("Bear buff");
+        movementSpeedMax = bearMovementSpeedMax;
+        jumpForce = bearJumpForce;
+        acceleration = bearAcceleration;
+        deceleration = bearDeceleration;
+        turnRate = bearTurnRate;
+
+        yield return new WaitForSeconds(bearBuffTimer);
+        movementSpeedMax = humanMovementSpeedMax;
+        jumpForce = humanJumpForce;
+        acceleration = humanAcceleration;
+        deceleration = humanDeceleration;
+        turnRate = humanTurnRate;
+
+        Debug.Log("Bear buff finished");
+    }
+
+    
+}  
