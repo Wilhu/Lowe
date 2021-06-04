8d04799b23ab139a866b88948d91f03907b29c07
Wilhu <toni.holappa@hotmail.fi>
2021-06-04 12:58:43 +0300

    deathpanel

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -23,7 +23,7 @@ public class PlayerHealth : MonoBehaviour
         {
             Debug.Log("ded");
             StartCoroutine(FadeBlack.GetComponent<FadeBlack>().ScreenFadeBlack());
-            Time.timeScale = 0f;
+            Time.timeScale = 0.1f;
             DeathPanel.SetActive(true);
             //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
